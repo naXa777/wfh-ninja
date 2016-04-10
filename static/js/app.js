202f705c497901fe29bd9169def0276e633967ee
naXa! <alarmo.lord@gmail.com>
2016-04-10 13:47:17 +0300

    Add "fork me" badge

--- static/js/app.js
+++ static/js/app.js
@@ -34,11 +34,18 @@ var Main = React.createClass({
   render: function() {
     return (
         <div className="site-wrapper">
+          <a href="https://github.com/naXa777/wfh-ninja" target="_blank">
+            <img style={{position: 'absolute', top: 0, right: 0, border: 0}}
+                 src="https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67"
+                 alt="Fork me on GitHub"
+                 data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" />
+          </a>
           <Quotes />
           <SubmitForm />
-          <p className="ack">Original <a href="http://getbootstrap.com/examples/cover/" target="_blank">Cover</a>
-            template for <a href="http://getbootstrap.com" target="_blank">Bootstrap</a> created by <a
-                href="https://twitter.com/mdo" target="_blank">@mdo</a>.</p>
+          <p className="ack">Original <a
+              href="http://getbootstrap.com/examples/cover/" target="_blank">Cover</a> template for <a
+              href="http://getbootstrap.com" target="_blank">Bootstrap</a> created by <a
+              href="https://twitter.com/mdo" target="_blank">@mdo</a>.</p>
           <div className="mastfoot clearfix">
             <div className="inner">
               <nav>
@@ -46,6 +53,7 @@ var Main = React.createClass({
                   <li><a href="#">
                     <div onClick={this.scrollDown}>Добавить</div>
                   </a></li>
+                  <li>О сайте</li>
                 </ul>
               </nav>
             </div>
